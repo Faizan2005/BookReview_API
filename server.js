3bb7d5ba16a00a2de4638e86ec996e2c1cbcebe5
Faizan2005 <illusionbuster05@gmail.com>
2025-06-04 22:35:44 +0530

    feat: add API routes

--- server.js
+++ server.js
@@ -13,6 +13,40 @@ server.listen(PORT, () => {
     console.log(`[Server] Visit http://localhost:${PORT}`)
 })
 
+app.post('/books', (req, res) => {
+    
+})
+
+app.get('/books', (req, res) => {
+
+})
+
+app.get('/books/:author', (req, res) => {
+
+})
+
+app.get('/books/:genre', (req, res) => {
+
+})
+
+
+app.get('/books/:id', (req, res) => {
+
+})
+
+app.post('/books/:id/reviews', (req, res) => {
+
+})
+
+app.put('/reviews/:id', (req, res) => {
+
+})
+
+app.delete('/reviews/:id', (req, res) => {
+    
+})
+
+
 
 
 
